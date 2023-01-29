57ecdfa8ccd0dd8f6eff8f1752358859b236d172
qrbaxter <96304553+qrbaxter@users.noreply.github.com>
2023-01-29 12:26:48 +1100

    Update index.js
    fixed replication issue

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
+const colorSection = document.getElementById("color-section")
 function getColors() {
   
   fetch(`https://www.thecolorapi.com/scheme?hex=${document.getElementById("color-input").value.substr(-6)}&mode=${document.getElementById("mode").value}&count=5`)
       .then(res => res.json())
       .then(data => {
+        colorSection.innerHTML = ''
         for(let color of data.colors) {
-          document.getElementById("color-section").innerHTML +=  `
+          color.innerHTML +=  `
             <div class="color-value">
               <img class="color-img" src="${color.image.bare}">
               <h3>${color.hex.value}</h3>
